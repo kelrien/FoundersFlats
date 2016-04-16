d2374e8e0900deb77502a11f55849c9553df679f
Wasili Adamow <wasili.adamow@gmail.com>
2016-04-16 17:05:29 +0200

    added features to offers site

--- templates/js/offer.js
+++ templates/js/offer.js
@@ -1,5 +1,6 @@
 var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var labelIndex = 0;
+var markerlist = {};
 
 function initMap() {
 
@@ -10,15 +11,56 @@ function initMap() {
   });
 
   addMarker({lat: 49.487760, lng: 8.466242}, map);
+  addMarker({lat: 49.489015, lng: 8.472250}, map);
 }
 
 // Adds a marker to the map.
 function addMarker(location, map) {
   // Add the marker at the clicked location, and add the next-available label
   // from the array of alphabetical characters.
+  var l = labels[labelIndex++ % labels.length];
   var marker = new google.maps.Marker({
     position: location,
-    label: labels[labelIndex++ % labels.length],
-    map: map
+    label: l,
+    map: map,
+            icon: normalIcon()
   });
+  markerlist[l] = marker;
+
+  marker.addListener('click', function() {
+    scrollToEntry(marker.getLabel());
+  });
+}
+
+function scrollToEntry(id) {
+  $("#entrylist").animate({scrollTop: $("#entry-"+id).offset().top-$("#entrylist").offset().top}, 'fast');
+  $("#entry-"+id).css("backgroundColor", "#eee");
+  $("#entry-"+id).animate({backgroundColor: "#fff"}, 700 );
+}
+
+$("#entrylist .entry").hover(
+  function() {
+        markerlist[$(this)[0].id.replace("entry-", "")].setIcon(highlightedIcon());
+  }, function() {
+        markerlist[$(this)[0].id.replace("entry-", "")].setIcon(normalIcon());
+  }
+);
+
+function toggleBounce(marker, start) {
+  if (start) {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  } else {
+    marker.setAnimation(null);
+  }
+}
+// functions that return icons.  Make or find your own markers.
+function normalIcon() {
+  return {
+    url: 'media/marker.png'
+  };
+}
+function highlightedIcon() {
+  return {
+    url: 'media/marker-active.png'
+  };
 }
